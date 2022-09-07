d4abddc9ef600952e73d15518d0dd66614815953
Matheus Felizardo <mfelizardo@envivent.com>
2022-09-06 23:36:34 -0300

    removing header in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,7 @@
 import {AppProps} from 'next/app';
-import {Router} from 'next/router'
 import React from 'react';
-import { Header } from '../components/Header';
-import NProgress from 'nprogress'
 import '../styles/global.scss';
 import '../styles/nprogress.scss';
-// Router.events.on('routeChangeStart', (url) => {
-//   NProgress.start();
-// })
-
-// Router.events.on('routeChangeComplete', () => { NProgress.done(); })
-// Router.events.on('routeChangeError', () => { NProgress.done(); })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
