8822b91b0176224f89da004f685f672b9422b96c
Matheus Felizardo <mfelizardo@envivent.com>
2022-10-18 16:17:56 -0300

    removing comments

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,27 +2,6 @@ import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitia
 import { useTheme } from '../contexts/ThemeContext';
 import { ServerStyleSheet } from 'styled-components'
 
-// export default class MyDocument extends Document {
-//     render() {
-//         return (
-//             <Html>
-//                 <Head>
-//                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-//                 <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;500;700&family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
-                
-//                 <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-                
-//                 </Head>
-
-//                 <body>
-//                     <Main />
-//                     <NextScript />
-//                 </body>
-//             </Html>
-//         )
-//     }
-// }
-
 export default class MyDocument extends Document {
     static async getInitialProps(
       ctx: DocumentContext
