2a63f3d9e4d91f8a6e979282b974ef9aefc4b380
Matheus Felizardo <mfelizardo@envivent.com>
2022-10-18 16:17:41 -0300

    server side rendering the styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ import Matheus from '/public/images/matheus.svg'
 import Rodrigues from '/public/images/rodrigues.svg'
 import Felizardo from '/public/images/felizardo.svg'
 import { StyledContainer, StyledLoading, StyledIntro} from '../styles/_app.style'
+import Head from 'next/head';
 const { Content } = Layout;
 
 export interface PageProps {
@@ -64,51 +65,59 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [language, loading])
 
   return (
-    <CustomThemeProvider>
-      <GlobalStyle />
-      <StyledIntro id="intro" className={showIntro ? 'visible' : 'hidden'}>
-        <div className="initial-loading">
-          <svg xmlns="http://www.w3.org/2000/svg" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-            <circle cx="50" cy="50" r="32" strokeWidth="8" stroke={theme?.name === 'dark' ? '#93dbe9' : '#0d5acd'}  strokeDasharray="50.26548245743669 50.26548245743669" fill="none" strokeLinecap="round">
-              <animateTransform attributeName="transform" type="rotate" dur="2s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
-            </circle>
-            <circle cx="50" cy="50" r="23" strokeWidth="8" stroke={theme?.name === 'dark' ? '#689cc5' : 'var(--blue-500)'} strokeDasharray="36.12831551628262 36.12831551628262" strokeDashoffset="36.12831551628262" fill="none" strokeLinecap="round">
-              <animateTransform attributeName="transform" type="rotate" dur="2s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;-360 50 50"></animateTransform>
-            </circle>
-          </svg>
-        </div>
-        <div className="blue-left"></div>
-        <div className="blue-right"></div>
-        <div className="logo-wrapper">
-          <Matheus/>
-          <Rodrigues/>
-          <Felizardo/>
-        </div>
-      </StyledIntro>
-      
-      <StyledContainer className={initialShowContent ? 'visible' : 'hidden'}>
-        <Layout>
-          <MainMenu 
-            selectedKey={selectedKey} 
-            setLoading={setLoading} 
-          />
-          <Layout id="np-content" >
-            <Content>
-              {loading ? 
-                  <StyledLoading>
-                  <Spin indicator={<LoadingOutlined />} />
-                  </StyledLoading>
-                  :
-                    <Component 
-                      setSelectedKey={setSelectedKey} 
-                      {...pageProps} 
-                    />
-                }
-            </Content>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;500;700&family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
+                        
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <CustomThemeProvider>
+        <GlobalStyle />
+        <StyledIntro id="intro" className={showIntro ? 'visible' : 'hidden'}>
+          <div className="initial-loading">
+            <svg xmlns="http://www.w3.org/2000/svg" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+              <circle cx="50" cy="50" r="32" strokeWidth="8" stroke={theme?.name === 'dark' ? '#93dbe9' : '#0d5acd'}  strokeDasharray="50.26548245743669 50.26548245743669" fill="none" strokeLinecap="round">
+                <animateTransform attributeName="transform" type="rotate" dur="2s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
+              </circle>
+              <circle cx="50" cy="50" r="23" strokeWidth="8" stroke={theme?.name === 'dark' ? '#689cc5' : 'var(--blue-500)'} strokeDasharray="36.12831551628262 36.12831551628262" strokeDashoffset="36.12831551628262" fill="none" strokeLinecap="round">
+                <animateTransform attributeName="transform" type="rotate" dur="2s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;-360 50 50"></animateTransform>
+              </circle>
+            </svg>
+          </div>
+          <div className="blue-left"></div>
+          <div className="blue-right"></div>
+          <div className="logo-wrapper">
+            <Matheus/>
+            <Rodrigues/>
+            <Felizardo/>
+          </div>
+        </StyledIntro>
+        
+        <StyledContainer className={initialShowContent ? 'visible' : 'hidden'}>
+          <Layout>
+            <MainMenu 
+              selectedKey={selectedKey} 
+              setLoading={setLoading} 
+            />
+            <Layout id="np-content" >
+              <Content>
+                {loading ? 
+                    <StyledLoading>
+                    <Spin indicator={<LoadingOutlined />} />
+                    </StyledLoading>
+                    :
+                      <Component 
+                        setSelectedKey={setSelectedKey} 
+                        {...pageProps} 
+                      />
+                  }
+              </Content>
+            </Layout>
           </Layout>
-        </Layout>
-      </StyledContainer>
-    </CustomThemeProvider>
+        </StyledContainer>
+      </CustomThemeProvider>
+    </>
   )
 }
 
