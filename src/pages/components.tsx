9a37940a71cebdef8224d3aead15e947155640d3
Matheus Felizardo <mfelizardo@envivent.com>
2022-10-19 21:06:46 -0300

    adding metatags

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -14,7 +14,12 @@ const PageComponent = ({ setSelectedKey }: PageProps) => {
   return (
     <>
       <Head>
-          <title>{`${translate("Components")} | ${translate("Matheus Felizardo - Front-end developer")}`}</title>
+        <meta property="og:image" content="/images/mrf-logo.png" />
+        <meta property="og:title" content="Matheus Rodrigues Felizardo | Front-end developer - Components" />
+        <meta property="og:description" content="Here you can find some components powered by Matheus Rodrigues Felizardo | Aqui você encontra alguns componentes desenvolvidos por Matheus Rodrigues Felizardo" />
+        <meta property="og:url" content="https://www.matheusfelizardo.com.br/projects" />
+        <meta name="description" content="Here you can find some components powered by Matheus Rodrigues Felizardo | Aqui você encontra alguns componentes desenvolvidos por Matheus Rodrigues Felizardo" />
+        <title>{`${translate("Components")} | ${translate("Matheus Felizardo - Front-end developer")}`}</title>
       </Head>
     
       < Components />
